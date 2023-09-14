60d9fc2abe7c76eeccbfa9054ccefd90f180e725
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-09-14 04:44:32 -0400

    Organization, final touches
    - organized components in folders
    - adjusted camera angles for the build
    - adjusted the sound volume

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -40,7 +40,7 @@ void RespondToDebugKey()
     }
     else if (Input.GetKeyDown(KeyCode.C))
     {
-        //(this will toggle collision/bool) collisionDisable = !collisionDisable; 
+        collisionDisable = !collisionDisable; //(this will toggle collision/bool)
     } //disabling collision is good for testing the level out adding cheat keys
 }
 
